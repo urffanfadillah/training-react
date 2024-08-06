664a4454191f61e39c21924535eb1eaf19dca8f1
urffanfadillah <urffan.dev@gmail.com>
2024-08-06 20:11:08 +0700

    removing console.log

--- src/App.jsx
+++ src/App.jsx
@@ -6,8 +6,6 @@ import "./App.css";
 function App() {
   const [count, setCount] = useState(0);
 
-  console.log('asd');
-
   return (
     <>
       <div>
@@ -18,7 +16,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1>Vite + Urffan</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
